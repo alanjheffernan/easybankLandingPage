466ffa19048677d6b253c8e87a148e08eecfd8f9
Alan Heffernan <alan.heffernan@sky.uk>
2020-06-30 08:49:20 +0100

    added article section without responsivness

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ window.addEventListener('click', function(){
     if(hamburgerDisplay == true){
         document.getElementById('nav-icon').src = "/images/icon-close.svg";
         document.querySelector(".nav-links").style.display = "block";
+        document.querySelector("#headlineSection").style.background = "linear-gradient(#2D314D, #2d314d00)";
     } else {
         document.getElementById('nav-icon').src = "/images/icon-hamburger.svg";
         document.querySelector(".nav-links").style.display = "none";
+        document.querySelector("#headlineSection").style.background = "transparent";
     }
     hamburgerDisplay = !hamburgerDisplay;
 });
@@ -18,8 +20,6 @@ window.addEventListener('click', function(){
 window.onresize = showNavItems();
 
 function showNavItems(){
-    console.log(window.innerWidth);
-    
     if(window.innerWidth > 700){
         document.querySelector(".nav-links").style.display = "block";
     } else {
